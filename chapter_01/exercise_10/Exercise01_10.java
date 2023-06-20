5751e67b5b955c0aa5ff9c72ff2f3941b9bf4123
Paula Rodrigues <pullynnhah@icloud.com>
2023-06-20 13:26:55 -0300

    chapter 1

---
+++ chapter_01/exercise_10/Exercise01_10.java
@@ -0,0 +1,7 @@
+package chapter_01.exercise_10;
+
+public class Exercise01_10 {
+  public static void main(String[] args) {
+    System.out.println((14 / 1.6) / ((45 * 60 + 30) / 3600.0));
+  }
+}
---
+++ chapter_01/exercise_12/Exercise01_12.java
@@ -0,0 +1,7 @@
+package chapter_01.exercise_12;
+
+public class Exercise01_12 {
+  public static void main(String[] args) {
+    System.out.println((24 * 1.6) / ((3600 + 40 * 60 + 35) / 3600.0));
+  }
+}
